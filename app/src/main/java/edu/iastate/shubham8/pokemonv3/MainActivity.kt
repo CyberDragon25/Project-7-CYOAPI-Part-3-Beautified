b36b4122f83b84cf1801e69caea18961b977dcbd
Shubham <bhattacharya.shubham123@gmail.com>
2023-11-02 22:49:31 -0500

    Created project and files

---
+++ app/src/main/java/edu/iastate/shubham8/pokemonv3/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.iastate.shubham8.pokemonv3
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
